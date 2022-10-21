76a331a8f5b05389b9a6d52b6dc17d128bc7cc38
Luuk Kenselaar <luuk@lkenselaar.dev>
2022-10-21 10:26:18 +0200

    Remove Mortgage- and Userid from MortgageResponseDTO

--- Models/DTO/MortgageResponseDTO.cs
+++ Models/DTO/MortgageResponseDTO.cs
@@ -3,8 +3,6 @@ namespace LKenselaar.CloudDatabases.Models.DTO
 {
     public class MortgageResponseDTO
     {
-        public Guid Id { get; set; }
         public double MaximumMortgage { get; set; }
-        public Guid UserId { get; set; }
     }
 }
